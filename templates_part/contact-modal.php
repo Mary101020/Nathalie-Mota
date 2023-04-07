120d73a79bdaff66b3e38737a053c2afbbb58894
Maria Cazacu <m.cazacu35@gmail.com>
2023-04-07 14:32:11 +0200

    ajustements finaux pour la modale, desktop et mobile

--- templates_part/contact-modal.php
+++ templates_part/contact-modal.php
@@ -1,12 +1,11 @@
-<!-- Trigger/Open The Modal -->
 <button id="myBtn">CONTACT</button>
 
 <!-- The Modal -->
 <div id="modal" class="modal">
   <div class="modal-content">
     <span class="close">&times;</span>
+    <div class="title"></div>
+    <div class="title1"></div>
     <?php echo do_shortcode( '[contact-form-7 id="39" title="Contact form"]' ); ?>
   </div>
 </div>
-
-
